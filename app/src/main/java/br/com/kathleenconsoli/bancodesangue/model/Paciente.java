fb30439f7776d613f23931de0d13f22ad57c74a4
kathleenconsoli <kathleenrauane@gmail.com>
2025-03-31 10:07:04 -0300

    Parado aqui

--- app/src/main/java/br/com/kathleenconsoli/bancodesangue/model/Paciente.java
+++ app/src/main/java/br/com/kathleenconsoli/bancodesangue/model/Paciente.java
@@ -1,9 +1,15 @@
 package br.com.kathleenconsoli.bancodesangue.model;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 
 public class Paciente {
     //atributos
@@ -24,13 +30,13 @@ public class Paciente {
             this.setUltimadoacao(jp.getString("ultima_doacao"));
             this.setTiposanguineo(jp.getString("tipo_sanguineo"));
             } catch (Exception e) {
-           Log.e("Paciente", e.getMessage());
+           Log.e("Paciente", Objects.requireNonNull(e.getMessage()));
         }
     }
     //CONSTRUTOR - Inicializa os atributos para gerar Objeto Json
     public Paciente () {
-        this.setNomePaciente(" ");
-        this.setDatadenascimento(" ");
+        this.setNomePaciente("");
+        this.setDatadenascimento("1970-01-01");
         this.setSexo("");
         this.setJadoouantes("");
         this.setUltimadoacao("");
@@ -40,14 +46,14 @@ public class Paciente {
     public JSONObject toJsonObject() {
         JSONObject json = new JSONObject();
         try {
-            json.put("id", this.nomePaciente);
-            json.put("nome", this.datadenascimento);
-            json.put("senha", this.sexo);
-            json.put("email", this.jadoouantes);
-            json.put("aceite", this.ultimadoacao);
-            json.put("nascimento", this.tiposanguineo);
+            json.put("nomePaciente", this.nomePaciente);
+            json.put("datadenascimento", this.datadenascimento);
+            json.put("sexo", this.sexo);
+            json.put("jadoouantes", this.jadoouantes);
+            json.put("ultimadoacao", this.ultimadoacao);
+            json.put("tiposanguineo", this.tiposanguineo);
         } catch (JSONException e) {
-            Log.e("Paciente", e.getMessage());
+            Log.e("Paciente", Objects.requireNonNull(e.getMessage()));
         }
         return json;
     }
@@ -56,50 +62,39 @@ public class Paciente {
     public String getNomePaciente(){
         return this.nomePaciente;
     }
-    public void setNomePaciente(String np){
-        if(np.length() > 4 && !np.contains("1,2,3,4,5,6,7,8,9,0,@")) {
-            this.nomePaciente = np;
-        }else {
-            this.nomePaciente = "nome_paciente";
-        }
-    }
+    public void setNomePaciente(String nomePaciente){this.nomePaciente = nomePaciente;}
 
     public String getDatadenascimento() {
         return this.datadenascimento;
     }
-
     public void setDatadenascimento(String datadenascimento) {
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formato =
+                new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date data = (Date) formato.parse(datadenascimento);
+//se chegar até aqui não deu erro no parser
         this.datadenascimento = datadenascimento;
+    } catch (ParseException e) {
+            this.datadenascimento = "1900-01-01";
+        }
     }
 
-    public String getSexo() {
-        return sexo;
-    }
-
+    public String getSexo() {return sexo;}
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
 
-    public String getJadoouantes() {
-        return jadoouantes;
-    }
-
+    public String getJadoouantes() {return jadoouantes;}
     public void setJadoouantes(String jadoouantes) {
         this.jadoouantes = jadoouantes;
     }
 
-    public String getUltimadoacao() {
-        return ultimadoacao;
-    }
-
+    public String getUltimadoacao() {return ultimadoacao;}
     public void setUltimadoacao(String ultimadoacao) {
         this.ultimadoacao = ultimadoacao;
     }
 
-    public String getTiposanguineo() {
-        return tiposanguineo;
-    }
-
+    public String getTiposanguineo() {return tiposanguineo;}
     public void setTiposanguineo(String tiposanguineo) {
         this.tiposanguineo = tiposanguineo;
     }
