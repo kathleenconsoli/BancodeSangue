de063590892d40a39edf5e0d1d4baa969ab4cbb8
kathleenconsoli <kathleenrauane@gmail.com>
2025-03-31 11:37:44 -0300

    Click do botão Cadastrar verificar o ultimo erro

--- app/src/main/java/br/com/kathleenconsoli/bancodesangue/ui/paciente/CadPacienteFragment.java
+++ app/src/main/java/br/com/kathleenconsoli/bancodesangue/ui/paciente/CadPacienteFragment.java
@@ -7,15 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import br.com.kathleenconsoli.bancodesangue.R;
+import br.com.kathleenconsoli.bancodesangue.model.Paciente;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CadPacienteFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CadPacienteFragment extends Fragment {
+public class CadPacienteFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +30,16 @@ public class CadPacienteFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private View view;
+
+    //Objetos de tela
+    private EditText etnome;
+    private EditText etdatadenascimento;
+    private RadioGroup rgSexo;
+    private RadioGroup rgjadoou;
+    private EditText etDoacaoData;
+    private Spinner sptipos_sanguineo;
+    private Button btsalvar;
 
     public CadPacienteFragment() {
         // Required empty public constructor
@@ -61,6 +76,48 @@ public class CadPacienteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cad_paciente, container, false);
+        this.view =  inflater.inflate(R.layout.fragment_cad_paciente, container, false);
+
+        //Binding (vinculo entre a tela e o objeto
+        this.etnome = view.findViewById(R.id.etnome);
+        this.etdatadenascimento = view.findViewById(R.id.etdatadenascimento);
+        this.rgSexo = view.findViewById(R.id.rgSexo);
+        this.rgjadoou = view.findViewById(R.id.rgjadoou);
+        this.etDoacaoData = view.findViewById(R.id.etDoacaoData);
+        this.sptipos_sanguineo = view.findViewById(R.id.sptipos_sanguineo);
+        this.btsalvar = view.findViewById(R.id.btsalvar);
+
+        //definindo o listener do botão
+        this.btsalvar.setOnClickListener(this);
+
+        //retorno da função
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btsalvar) {
+
+            try {
+                //instanciar meu objeto de negócio
+                Paciente paciente = new Paciente();
+                //Pegar os dados da tela e por no objeto
+                paciente.setNomePaciente(this.etnome.getText().toString());
+                paciente.setDatadenascimento(this.etdatadenascimento.getText().toString());
+                int radioButtonSexo =
+                        rgSexo.getCheckedRadioButtonId();
+                View radioButtonsexo = rgSexo.findViewById(radioButtonSexo);
+                paciente.setSexo(String.valueOf(rgSexo.indexOfChild()));
+                int radioButtonJadoou =
+                        rgjadoou.getCheckedRadioButtonId();
+                View radioButtonjadoou = rgjadoou.findViewById(radioButtonJadoou);
+                paciente.setJadoouantes(String.valueOf(rgjadoou.indexOfChild(radioButtonjadoou)));
+                paciente.setUltimadoacao(this.etDoacaoData.getText().toString());
+                paciente.setSexo(String.valueOf(this.sptipos_sanguineo.getSelectedItemPosition()));
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
